4da414172084061b8686165ed07ee6afd88157fe
Macarena <macarenagonzalezalonso@gmail.com>
2021-06-04 14:06:10 +0200

    add conditional collapsibles

--- src/components/Collapsable.js
+++ src/components/Collapsable.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 function Collapsable(props) {
+  //true es abierto
+  const [isDesing, setDesing] = useState(true);
+  const [isFill, setFill] = useState();
+  const [isShare, setShare] = useState();
+  
+  const handleOnClick = () => {
+    if (isDesing === true){
+      setDesing(false)
+    }else{
+      setDesing(true)
+      setFill(false)
+      setShare(false)
+    }
+  };
+  const collapsibleChange = isDesing === true || isFill === true || isShare === true? "active" : "";
+  
   return (
-    <section className="design active js-section">
-      <div className="title_wrapper">
+    <section className={`design ${collapsibleChange} js-section`}>
+      <div className="title_wrapper " onClick={handleOnClick}>
         <div className="title_wrapper--text">
           <i className={props.icon}></i>
           <h2 className="title">{props.title}</h2>
